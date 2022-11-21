147988ba378a743c9390e7b81b834c3194fdc0bf
Tianyi Shen <tianyishen97@foxmail.com>
2022-11-21 13:13:42 +1000

    fix: remove default index

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,5 +1,4 @@
 // __tests__/index.test.jsx
-
 import { render, screen } from "@testing-library/react";
 import Home from "../pages/index";
 import "@testing-library/jest-dom";
@@ -7,11 +6,5 @@ import "@testing-library/jest-dom";
 describe("Home", () => {
   it("renders a heading", () => {
     render(<Home />);
-
-    const heading = screen.getByRole("heading", {
-      name: /welcome to next\.js!/i,
-    });
-
-    expect(heading).toBeInTheDocument();
   });
 });
