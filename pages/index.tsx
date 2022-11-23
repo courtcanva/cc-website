ca24933f066d19a68d654c498a2d099e749524d4
Cecilia0109 <100055462+Cecilia0109@users.noreply.github.com>
2022-11-24 00:55:54 +1030

    fix: fix fonot family init

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,10 @@
 import HeroContainer from "../components/hero/HeroContainer";
 import WhyChooseUs from "../components/WhyChooseUs";
-import {Poppins } from '@next/font/google'
 
-const poppins = Poppins({weight: ['300','400','500','600','700','800','900']})
 
 export default function Home() {
-  return <div className={poppins.className}>
+  return <>
   <HeroContainer />
   <WhyChooseUs />
-  </div>;
+  </>;
 }
