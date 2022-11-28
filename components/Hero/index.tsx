3d24bff172a4b6ed8ecb386f85e8d4747f778825
Cecilia0109 <100055462+Cecilia0109@users.noreply.github.com>
2022-11-28 11:30:58 +1030

    fix: delete unused files

---
+++ components/Hero/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const Hero = () => {
+  return (
+    <section className="max-w-7xl  bg-blue flex flex-col margin-auto md:py-[106px] lg:py-[143px]  md:w-[100vw] relative  mx-auto border-white text-white overflow-hidden">
+   
+    </section>
+
+  )
+}
+
+export default Hero
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-import HeroContainer from "../components/hero/HeroContainer";
 import WhyChooseUs from "../components/WhyChooseUs";
 import Intro from "../components/Intro";
 import Template from "../components/Template";
@@ -6,7 +5,6 @@ import Template from "../components/Template";
 export default function Home() {
   return (
     <>
-      <HeroContainer />
       <WhyChooseUs />
       <Intro />
       <Template />
