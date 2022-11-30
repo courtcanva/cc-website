c32d2b6c35d8b45fbe0247b6e8ec5eeaab72c412
PengchengYuGithub <111633338+PengchengYuGithub@users.noreply.github.com>
2022-11-30 11:01:57 +0800

    Feat/cc 145 advantages landing page (#9)
    * feat: landing page advantages section

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,14 @@ import WhyChooseUs from "../components/WhyChooseUs";
 import Intro from "../components/Intro";
 import Template from "../components/Template";
 import Hero from "../components/Hero";
+import Advantages from "../components/Advantages";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <WhyChooseUs />
+      <Advantages />
       <Intro />
       <Template />
     </>
